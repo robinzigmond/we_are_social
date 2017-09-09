a92d0d36d3481ff5f7ec9b4c190e193c957dcbec
Robin Zigmond <robinzig@hotmail.com>
2017-09-09 21:32:02 +0100

    updated settings, and removed blank line from Procfile

--- settings/staging.py
+++ settings/staging.py
@@ -14,7 +14,7 @@ STRIPE_PUBLISHABLE = os.getenv("STRIPE_PUBLISHABLE", "pk_test_AjjVRbuE0roWmK3bOF
 STRIPE_SECRET = os.getenv("STRIPE_SECRET", "sk_test_hSOzgeTqJC6jlTq12c3L9Gdq")
 
 # PayPal Settings:
-SITE_URL = "http://127.0.0.1:8000"
+SITE_URL = "https://rz-we-are-social-staging.herokuapp.com/"
 if DEBUG:
     PAYPAL_NOTIFY_URL = "http://rz-we-are-social-staging.herokuapp.com/a-very-hard-to-guess-url/"
 PAYPAL_RECEIVER_EMAIL = "robinzigmond@gmail.com"
